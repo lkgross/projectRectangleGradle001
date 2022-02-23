4eabbb621c7cdfd6b1deed36713aabb6b1f4d024
L1Gross <profgross@gmail.com>
2022-02-23 15:14:16 -0500

    Overload constructors, copy an object

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -5,8 +5,17 @@ package bsu.comp152;
  */
 public class Main {
     public static void main(String[] args) {
+        // Do an inline initialization of a Rectangle object rect1.
+        // We say rect1 is an instance of the Rectangle class.
+        Rectangle rect1 = new Rectangle(5.77832, 2.31071);
+        System.out.printf("The rectangle has approximate dimensions %f x %f \n", rect1.getLength(),
+                rect1.getWidth());
+        System.out.printf("The rectangle has approximate dimensions %6.2f x %6.2f\n", rect1.getLength(),
+                rect1.getWidth());
 
-        Rectangle rect1 = new Rectangle(5.77, 2.3);
+        // DON'T do a reference copy like
+        Rectangle rect3 = rect1;
+
         /*
          * After we write a constructor that takes
          * two parameters, the default constructor
@@ -20,16 +29,59 @@ public class Main {
         // If you try to print an object,
         // then Java will automatically look for a
         // toString method.
-        System.out.println(rect1);
+        System.out.println(rect3);
 
-        // rect1.setLength(5.77);
-        System.out.println(rect1.getLength());
-        System.out.println(rect1.getWidth());
+        rect1.setLength(5.77);
+
+        // Because of the reference copy, you can't alter rect1 without altering rect3.
+        System.out.println(rect1);
+        System.out.println(rect3);
+
+        Rectangle rect4 = new Rectangle();
+
+        System.out.println(rect1);
+        System.out.println(rect4);
+
+        // One good way to copy is with a copy method.
+        // Overwrite rect4 with a copy of rect1.
+        rect4.copy(rect1);
+
+        System.out.println(rect1);
+        System.out.println(rect4);
+
+        // Now we can alter rect1 without altering rect3.
+        rect1.setLength(2);
+
+        System.out.println(rect1);
+        System.out.println(rect4);
+
+        // Another good way to copy is with a copy constructor.
+        Rectangle rect5 = new Rectangle(rect1);
+
+        System.out.println(rect1);
+        System.out.println(rect5);
 
         Rectangle rect2 = new Rectangle(5.77, 2.3);
         // Don't use == to compare objects!!
         System.out.println(rect1 == rect2);
         // Use an equals method.
         System.out.println(rect1.equals(rect2));
+
+        /* Let's do comparisons between creating and manipulating
+         * Rectangle objects and creating and manipulating String
+         * objects.
+         */
+        String name = "Laura";
+        String name2 = new String("Laura");
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name.toLowerCase());
+        System.out.println(name2.toUpperCase());
+        System.out.println(name);
+        System.out.println(name2);
+        // Don't use == to compare objects!!
+        System.out.println(name == name2);
+        // Use an equals method.
+        System.out.println(name.equals(name2));
     }
 }
--- src/main/java/bsu/comp152/Rectangle.java
+++ src/main/java/bsu/comp152/Rectangle.java
@@ -23,19 +23,60 @@ public class Rectangle {
      * The name of the constructor is same
      * as the name of the class!
      */
-    public Rectangle(double len, double wid){
-        length = len;
-        width = wid;
+    public Rectangle(double length, double width){
+        /* If you use parameter names that are the
+         * same as the instance variable names,
+         * then use the reference variable this
+         * to distinguish between them.
+         */
+        this.length = length;
+        this.width = width;
+    }
+
+    // We can do constructor overloading: write more
+    // than one constructor.
+    // Their "signatures" must differ:
+    // They have the same name, but they need a different
+    // number of parameters or different types of parameters.
+    public Rectangle(){
+        // One constructor can call another using this
+        // in the first line:
+        this(0.0, 0.0);
+        // Another way is to use:
+        // length = 0.0;
+        // width = 0.0;
+    }
+
+    // Write a copy constructor:
+    public Rectangle(Rectangle other){
+        length = other.length;
+        width = other.width;
+        // Alternatively we could have the body of the
+        // constructor as:
+        // this(other.length, other.width);
     }
 
     // Here are mutator or setter instance methods:
 
-    public void setLength(double len){
+    /* public void setLength(double len){
         length = len;
+    }*/
+
+    /* We can use the same name for the parameter
+     * as for the instance variable if we use the
+     * reference variable this.
+     */
+    public void setLength(double length){
+        this.length = length;
     }
 
     public void setWidth(double wid){
         width = wid;
+    }
+
+    public void copy(Rectangle other){
+        length = other.length;
+        width = other.width;
     }
 
     // Here are accessor or getter instance methods:
@@ -50,8 +91,8 @@ public class Rectangle {
 
     @Override
     public String toString(){
-        return ("length = " + length + ", width = " +
-                + width);
+        return String.format("length = %f, width = %f",
+                length, width);
     }
 
     public boolean equals(Rectangle other){
